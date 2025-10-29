5b6f12ce136cbc717807699e3332fa5db25d13da
narava venkat siddharth <143152859+sid1651@users.noreply.github.com>
2025-10-29 13:08:26 +0530

    Change API base URL to production environment

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL, // backend base URL
+  baseURL: import.meta.env.VITE_PROD_URL, // backend base URL
 });
 
 export default api;
